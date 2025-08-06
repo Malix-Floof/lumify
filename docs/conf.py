d66242e69de9e7e36f858bb1bb1940a03655f81a
Malix <101697781+Malix-Floof@users.noreply.github.com>
2025-08-07 01:38:49 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,25 @@
 project = "lumify"
+
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.linkcode",
     "myst_parser",
+]
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
 ]
 
 html_theme = "furo"
